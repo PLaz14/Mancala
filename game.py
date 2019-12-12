9172e9a6a61d11f2a65699e28140f3902e9e1c08
Peter <greekgeeklazos@gmail.com>
2019-12-12 00:05:40 -0500

    can print the board successfully

--- game.py
+++ game.py
@@ -1,14 +1,26 @@
-import numpy as np
+# import numpy as np
 
 
-class Board():
-    def __int__(self):
-        self.p2goal = 0
+class Board:
+    def __init__(self, b):
+        self.board = b
 
     def __str__(self):
-        return self.p2goal
+        s = '--'
+        for i in self.board[0]:
+            s += '{:>3}'.format(i)
+        s += '--\n' + '{:>2}'.format(self.board[1][0]) + ' | ' * 6 + '{:<2}'.format(self.board[1][1]) + '\n--'
+        for i in self.board[2]:
+            s += '{:>3}'.format(i)
+        s+='--'
+        return s
+
+    def move(self, pos):
+
 
 
 if __name__ == "__main__":
-    b = Board()
-    print(b)
+    startboard = [[4] * 6, [0] * 2, [4] * 6]
+    b = Board(startboard)
+
+
